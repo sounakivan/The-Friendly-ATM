ef48e6018a10d24c92f1827cdba226c54ec83196
Sounak Ghosh <sounaktheghosh@gmail.com>
2020-12-01 03:20:54 -0500

    added art assets and made atm screen

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let video;
 let poseNet;
 let poses = [];
 
-let cx, cy;
+let cx = 400, cy = 250;
 let r = 20;
 
 let speechRec;
@@ -14,17 +14,22 @@ let currentOptions;
 let ATMstart = false;
 
 let openingView;
+let atmScreen;
+let micIcon;
 let myFont;
 
 function preload() {
     openingView = loadImage('assets/opening view.png');
+    atmScreen = loadImage('assets/atm-screen-view.png');
     myFont = loadFont('assets/PressStart2P-Regular.ttf');
+    micIcon = loadImage('assets/mic-icon.png');
 }
 
 function setup() {
     createCanvas(800, 500);
     video = createCapture(VIDEO);
     video.hide();
+    pixelDensity(1);
     
     let options = {
         flipHorizontal: true
@@ -41,7 +46,7 @@ function setup() {
     let interim = false;
     speechRec.start(continuous, interim);
     
-    ATMsays = 'Want Cash?'
+    ATMsays = 'Hi there, welcome human. So you want cash?'
     currentOptions = wantCash;
 
 }
@@ -64,7 +69,7 @@ function drawCursor() {
         cx = mouthPos.x;
         cy = mouthPos.y + 50;
         noStroke();    
-        fill(0, 255, 0, 126);
+        fill(255, 150, 0, 126);
         ellipse(cx, cy, r, r);
     }
 }
@@ -74,53 +79,61 @@ function draw() {
     //image(video, width/2-120, 360, 240, 180);
     
     if (ATMstart === false) {
-        openingScreen();
+        showBackAlley();
     } else {
         showATMscreen();
+        ATMreact();
         drawCursor();
     }
-    
 }
 
-function openingScreen() {
+function showBackAlley() {
     image(openingView, 0, 0, width, height);
     
+    //UI
+    push();
     fill(255, 180);
     stroke(0);
     strokeWeight(4);
-    rect(40, 320, 275, 140, 10);
+    rect(40, 300, 300, 180, 10);
     
     fill(0);
-    noStroke();
-    textSize(16);
+    stroke(255);
+    strokeWeight(2);
+    textSize(18);
     textFont(myFont);
-    text('THE LONELY ATM', 60, 360);
+    text('THE LONELY ATM', 60, 340);
+    textSize(10);
+    text('Stop by for money, advice or just a chat.', 60, 355, 280, 100);
     
-    if (mouseX > 50 && mouseX < 130 && mouseY > 380 && mouseY < 440) {
+    //about button
+    if (mouseX > 60 && mouseX < 120 && mouseY > 400 && mouseY < 460) {
         fill(255, 255, 50);
     } else {
         fill(0, 100, 255);
     }
     stroke(0);
     strokeWeight(3);
-    rect(60, 380, 60, 60, 30);
+    rect(60, 400, 60, 60, 30);
     
     fill(255);
     textSize(24);
-    text('?', 80, 425);
+    text('?', 80, 445);
     
-    if (mouseX > 150 && mouseX < 300 && mouseY > 380 && mouseY < 440) {
+    //interact button
+    if (mouseX > 140 && mouseX < 290 && mouseY > 400 && mouseY < 460) {
         fill(50, 255, 255);
     } else {
         fill(200, 100, 255);
     }
     stroke(0);
     strokeWeight(3);
-    rect(140, 380, 150, 60, 10);
+    rect(140, 400, 150, 60, 10);
     
     fill(255);
     textSize(12);
-    text('Interact ->', 150, 415);
+    text('Interact ->', 150, 435);
+    pop();
 }
 
 function mousePressed() {
@@ -132,14 +145,59 @@ function mousePressed() {
 }
 
 function showATMscreen() {
-    let thisScreen = new ScreenState(ATMsays, width/2, 150, currentOptions);
+    image(atmScreen, 0, 0, width, height);
+    
+    //draw screen
+    push();
+    rectMode(CENTER);
+    fill(0, 200, 255);
+    stroke(0);
+    strokeWeight(3);
+    rect(width/2, height/2, 600, 350, 10);
+    noFill();
+    strokeWeight(2);
+    rect(width/2, height/2, 620, 370, 10);
+    
+    rectMode(CORNER);
+    noStroke();
+    fill(0, 255, 100);
+    rect(125, 100, 250, 150, 30);
+    stroke(0, 255, 100);
+    strokeWeight(2);
+    line(175, 250, 175, 300);
+    line(175, 300, 250, 350);
+    
+    if (poses.length > 0) {
+        let d = dist(cx, cy, 250, 350);
+        if (d < 50) {
+            fill(255, 100, 0);
+        } else {
+            fill(200, 200, 0);
+        }
+    }
+    noStroke();
+    ellipse(250, 350, 75);
+    image(micIcon, 225, 325, 50, 50);
+    pop();
+    
+    fill(0, 101, 68);
+    noStroke();
+    textFont(myFont);
+    textAlign(LEFT, CENTER);
+    textSize(45);
+    text('._.', 200, 175);
+}
+
+function ATMreact() {
+    let thisScreen = new ScreenState(ATMsays, currentOptions);
     thisScreen.display();
 }
 
 //ATM RESPONSE FUNCTIONS
 
 let enterPin = function() {
-    ATMsays = 'Enter your PIN'
+    
+    ATMsays = 'Enter your PIN.'
     currentOptions = pinPad;
 }
 
@@ -152,14 +210,13 @@ let whyNot = function() {
 
 let wantCash = function() {
     //generate selections
-    options.push(new UserSelection('Yes', width/2 - 200, height/2-50, enterPin));
-    options.push(new UserSelection('No', width/2, height/2-50, whyNot));
-    options.push(new UserSelection('Maybe', width/2 + 200, height/2-50, whyNot));
+    let optYes = new UserSelection('Yes', 550, 250, enterPin);
+    let optNo = new UserSelection('No', 550, 300, whyNot);
+    let optMaybe = new UserSelection('Maybe', 550, 350, whyNot);
     
-    for (let i = 0; i < options.length; i++) {
-        options[i].display();
-        options[i].detectSelect();
-    }
+    optYes.display();
+    optNo.display();
+    optMaybe.display();
 }
 
 let pinPad = function() {
@@ -171,19 +228,20 @@ let whyNotOps = function() {
 }
 
 class ScreenState {
-    constructor(screenText, xPos, yPos, genOptions) {
+    constructor(screenText, showOptions) {
         this.screenText = screenText;
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.genOptions = genOptions;
+        this.showOptions = showOptions;
         this.isState = false;
     }
     
     display() {
-        textSize(50);
+        textSize(20);
         fill(0);
-        text(this.screenText, this.xPos, this.yPos);
-        this.genOptions();
+        noStroke();
+        textFont(myFont);
+        textAlign(LEFT, TOP);
+        text(this.screenText, 550, 160, 300, 100);
+        this.showOptions();
     }
 }
 
@@ -202,28 +260,33 @@ class UserSelection {
     display() {
         rectMode(CENTER);
         //detect hover
-        if (poses) {
-            if (cx > this.xPos - 50 && cx < this.xPos + 50 && cy > this.yPos - 20 && cy < this.yPos + 20) {
-                noFill();
-                stroke(0,0,255)
-                rect(this.xPos, this.yPos, 110, 50, 5);
-                fill(0, 0, 255);
-                this.isSelected = true;
-            } else {
-                fill(0);
-            }
-        }
-        rect(this.xPos, this.yPos, 100, 40, 5);
+//        if (poses) {
+//            if (cx > this.xPos - 50 && cx < this.xPos + 50 && cy > this.yPos - 20 && cy < this.yPos + 20) {
+//                noFill();
+//                stroke(0,0,255)
+//                rect(this.xPos, this.yPos, 210, 50, 5);
+//                fill(0, 0, 255);
+//                this.isSelected = true;
+//            } else {
+//                fill(0, o, 255);
+//            }
+//        }
+        fill(0, 0, 255);
+        rect(this.xPos, this.yPos, 200, 40, 5);
         fill(255);
         noStroke();
         textSize(12);
-        textAlign(CENTER, CENTER);
-        text(this.optionText, this.xPos, this.yPos, 100, 40);
+        textFont(myFont);
+        textAlign(RIGHT, CENTER);
+        text(this.optionText, this.xPos, this.yPos, 200, 40);
+        
+        this.detectSelect();
     }
     
     detectSelect() {
         let lowercaseInput = this.optionText.toLowerCase();
         if (lowercaseInput === speechInput) {
+            
             this.onSelect();
         }
     }
